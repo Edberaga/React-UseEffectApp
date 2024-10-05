ee47c766ef34ebf7a7a5b8e955606eed472d934a
edberaga <edbetjonnathan@gmail.com>
2024-10-05 17:15:25 +0700

    Add the image

--- CRUD-ToDo-App/src/components/Header.jsx
+++ CRUD-ToDo-App/src/components/Header.jsx
@@ -1,15 +1,23 @@
 import { Container, Navbar } from 'react-bootstrap';
+import logo from '../assets/Site_Logo.png';
 
 const Header = () => {
   return (
-    <header>
-      <Navbar bg="light" variant='light'>
-        <Container>
-          <Navbar.Brand href='/'>Check Notes</Navbar.Brand>
-          <Navbar.Brand href='/add-note'>Add Notes</Navbar.Brand>
-        </Container>
-      </Navbar>
-    </header>
+  <header>
+    <div style={{
+      width: "100%",
+      backgroundColor: "white",
+      textAlign: "center"
+    }}>
+      <a href='/'><img src={logo} alt="To do list app logo" width="135px"/></a>
+    </div>
+    <Navbar className='pb-3' bg='white' variant='light'>
+      <Container>
+        <Navbar.Brand href='/note'>Check Notes</Navbar.Brand>
+        <Navbar.Brand href='/note/add-note'>Add Notes</Navbar.Brand>
+      </Container>
+    </Navbar>
+  </header>
   )
 }
 
